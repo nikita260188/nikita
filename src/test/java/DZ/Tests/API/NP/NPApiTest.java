c29d6c670925e3bf3496141fc7998cb312308708
Nikita <nik.pavlyk.88@gmail.com>
2020-09-19 15:42:36 +0300

    DZ20

--- src/test/java/DZ/Tests/API/NP/NPApiTest.java
+++ src/test/java/DZ/Tests/API/NP/NPApiTest.java
@@ -1,7 +1,6 @@
 package DZ.Tests.API.NP;
 
 import DZ.Tests.API.Privat.MethodNPHelper;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.HashMap;
@@ -9,14 +8,14 @@ import java.util.Map;
 
 public class NPApiTest extends MethodNPHelper {
 
-    @Ignore
+
     @Test
     public void getNP(){
         Map<String, Object> testData = new HashMap<>();
         Map<String, Object> Data = new HashMap<>();
         testData.put("modelName", "Common");
         testData.put("calledMethod", "getCargoTypes");
-        testData.put("apiKey", "a011a4e3b502bdb46765dfe0e73a2e7c");
+        testData.put("apiKey", "4c1a644fc298161e29aa815462d76255");
         testData.put("methodProperties",Data);
         post(testData, "/v2.0/common/getCargoTypes/json")
                 .then()
